fe683690ef053b4b3ef2bb69b7ff88031b99b31b
Alex H. Peraza <2020152022@ub.edu.bz>
2024-09-02 13:18:30 -0600

    Second version of the app

---
+++ cmd/api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const appVersion = "2.0.0"
+
+type serverConfig struct {
+	port        int
+	environment string
+}
+
+type applicationDependencies struct {
+	config serverConfig
+	logger *slog.Logger
+}
+
+func main() {
+	var setting serverConfig
+
+	flag.IntVar(&setting.port, "port", 4000, "Server port")
+	flag.StringVar(&setting.environment, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	appInstance := &applicationDependencies{
+		config: setting,
+		logger: logger,
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("/v1/healthcheck", appInstance.healthCheckHandler)
+
+	apiServer := &http.Server{
+		Addr:         fmt.Sprintf(":%d", setting.port),
+		Handler:      router,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("Starting server", "address", apiServer.Addr, "environment", setting.environment)
+	err := apiServer.ListenAndServe()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mtechguy/comments
+
+go 1.23.0
