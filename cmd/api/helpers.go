a04c65cee3c3556f5a43c25fe7b113d7e15c49fb
Alex H. Peraza <2020152022@ub.edu.bz>
2024-09-30 13:05:26 -0600

    encoded-json-v5

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 )
 
-func (a *applicationDependencies) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+type envelope map[string]any
+
+func (a *applicationDependencies) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	jsResponse, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
-
 	jsResponse = append(jsResponse, '\n')
 	// additional headers to be set
 	for key, value := range headers {
 		w.Header()[key] = value
-		//w.Header().Set(key, value)
 	}
 	// set content type header
 	w.Header().Set("Content-Type", "application/json")
