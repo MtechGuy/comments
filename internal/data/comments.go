20cb266ade3fe55fca54552309f73ea2354926fb
Alex H. Peraza <2020152022@ub.edu.bz>
2024-10-07 13:27:53 -0600

    parsing JSON client requests

---
+++ internal/data/comments.go
@@ -0,0 +1,15 @@
+// Filename: internal/data/comments.go
+package data
+
+import (
+	"time"
+)
+
+// each name begins with uppercase so that they are exportable/public
+type Comment struct {
+	ID        int64     // unique value for each comment
+	Content   string    // the comment data
+	Author    string    // the person who wrote the comment
+	CreatedAt time.Time // database timestamp
+	Verson    int32     // incremented on each update
+}
