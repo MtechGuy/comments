1463bc148eef6dcfe1bf1ce323f74a4d4aef0a8c
Alex H. Peraza <2020152022@ub.edu.bz>
2024-10-12 09:56:59 -0600

    database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const appVersion = "7.0.0"
@@ -14,6 +18,9 @@ const appVersion = "7.0.0"
 type serverConfig struct {
 	port        int
 	environment string
+	db          struct {
+		dsn string
+	}
 }
 
 type applicationDependencies struct {
@@ -26,9 +33,22 @@ func main() {
 
 	flag.IntVar(&setting.port, "port", 4000, "Server port")
 	flag.StringVar(&setting.environment, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&setting.db.dsn, "db-dsn", "postgres://comments:cattalya03@localhost/comments?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// the call to openDB() sets up our connection pool
+	db, err := openDB(setting)
+	if err != nil {
+		logger.Error("Database connection failed", err)
+		os.Exit(1)
+	}
+	// release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("Database connection pool established")
 
 	appInstance := &applicationDependencies{
 		config: setting,
@@ -45,9 +65,33 @@ func main() {
 	}
 
 	logger.Info("Starting server", "address", apiServer.Addr, "environment", setting.environment)
-	err := apiServer.ListenAndServe()
+	err = apiServer.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+
+}
+
+func openDB(settings serverConfig) (*sql.DB, error) {
+	// open a connection pool
+	db, err := sql.Open("postgres", settings.db.dsn)
 	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
+		return nil, err
 	}
+
+	// set a context to ensure DB operations don't take too long
+	ctx, cancel := context.WithTimeout(context.Background(),
+		5*time.Second)
+	defer cancel()
+
+	// let's test if the connection pool was created
+	// we trying pinging it with a 5-second timeout
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// return the connection pool (sql.DB)
+	return db, nil
+
 }
